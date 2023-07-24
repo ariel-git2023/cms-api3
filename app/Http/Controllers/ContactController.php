670b5367ff4044864ac94404c84020ab9f1e4338
Ariel Sto. Domingo <astodomingo@fullscale.io>
2023-07-24 16:42:49 +0800

    CRUD for Contacts

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // pull all data from contacts table
+        $contacts = Contact::all();
+        // return data as json
+        return response()->json($contacts);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // create new contact
+        $contact = Contact::create($request->all());
+        // return data as json
+        return response()->json($contact);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Contact $contact)
+    {
+        // create function for show one contact
+        return response()->json($contact);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Contact $contact)
+    {
+        // create update contact
+        $contact->update($request->all());
+        // return data as json
+        return response()->json($contact);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Contact $contact)
+    {
+        // create route for delete contact
+        $contact->delete();
+        // return data as json
+        return response()->json($contact);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+}); */
+
+
+Route::get('/contacts', [ContactController::class, 'index']);
+
+// create route for store contact
+Route::post('/contacts', [ContactController::class, 'store']);
+
+// create route for update
+Route::put('/contacts/{contact}', [ContactController::class, 'update']);
+
+// create route for delete
+Route::delete('/contacts/{contact}', [ContactController::class, 'destroy']);
+
+// create router for show
+Route::get('/contacts/{contact}', [ContactController::class, 'show']);
+
+
+
